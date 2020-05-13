0b3634535ae22d7497dcb32ed1d7bccdac9eaae7
corujasdevbr <fernando.guerra@corujasdev.com.br>
2020-05-13 09:56:54 -0300

    up

---
+++ App_About_Xamarin/App_About_Xamarin/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace App_About_Xamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Master.Layout();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App_About_Xamarin/App_About_Xamarin/Master/Layout.xaml.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_About_Xamarin.Master
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Layout : MasterDetailPage
+    {
+        public Layout()
+        {
+            InitializeComponent();
+        }
+
+        private void MudarPagina(object sender, EventArgs args)
+        {
+            string page = ((Button)sender).Text;
+
+            switch (page)
+            {
+                case "Miguel de Icaza":
+                case "Joseph Hill":
+                case "Nat Friedman":
+                    Detail = new NavigationPage(new Pages.Perfil1() { Title = page });
+                    IsPresented = false;
+                    break;
+                case "Sobre o Xamarin":
+                    Detail = new NavigationPage(new Pages.About() { Title = "Sobre" });
+                    IsPresented = false;
+                    break;
+            }
+        }
+    }
+}
